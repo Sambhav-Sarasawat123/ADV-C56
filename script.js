8be3a8a2a01b97c2d0a6a9867111f1a5b74a0524
Sambhav-Sarasawat123 <76476750+Sambhav-Sarasawat123@users.noreply.github.com>
2020-12-24 16:59:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function greet() {
+    var y = document.getElementById('Click-me')
+    var x = document.getElementById('textofmine');
+        x.style.display = 'block';
+        y.style.display = 'none';
+}
